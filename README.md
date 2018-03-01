0c7b639f231d98e1db68ace537462e9f44d7b8f4
1iedler <1iedler@users.noreply.github.com>
2018-03-01 23:18:23 +0100

    umbau auf aepfel und birnen film

--- README.md
+++ README.md
@@ -4,16 +4,8 @@ Hierbei handelt es sich um ein Experimentierprojekt.
 
 Das Projekt verwendet:
 
-technisch:
-
 * AngularJS v1.6.9, (c) 2010-2018 Google, Inc. http://angularjs.org
 
 * material-design-lite v1.3.0 Apache-2.0 (c) 2015 Google, Inc. https://github.com/google/material-design-lite
 
 * AngularJS Google Maps Ver. 1.18.4, The MIT License (MIT), (c) 2014, 2015, 1016 Allen Kim
-
-inhaltlich:
-
-* Texte und Bilder: Siehe Historischer Verein für Niederbayern e.V. IVHVN Band 140 (2014) MORITZ FISCHER "Eine größere Liebe hat niemand als der, der sein Leben hingibt ..." Die Freiheitsaktion Bayern in Landshut und der Mord an Franz Seiff 
-
-* Video: Curtesy of Big Buck Bunny https://peach.blender.org/
--- annotations/annotations.js
+++ annotations/annotations.js
@@ -1,74 +1,25 @@
 var vDataCharaktere =
 [
 	{
-		name: "Seiff",
-		vorname: "Franz",
-		kurzbeschreibung: "Der Gute.",
-		bild: "seiff.jpg",
-		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vSY_HQF75PgVtHE6Js2oCHUDdC3OO77fytnM-zZWXcmUGCFea_TDKdypcgD-DM3S1rifMgv9rmLs7Tr/pub", height: 3000}],
+		name: "",
+		vorname: "Apfel",
+		kurzbeschreibung: "Der Apfel kann sehr gut Rollen. Außerdem hat er viel Vitamin C.",
+		bild: "apfel.jpg",
+		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vQdzt7W13VUwv2SPCTfBUf6xnlvA_IIDOC9SWvwJn1ZQIXenW34EH4HGpaKHWh5KjNZaP9gf5wKJndv/pub", height: 600}],
 		timing: [
-			{ start: 7, end: 11 },
-			{ start: 13, end: 33 }
+			{ start: 4, end: 40 },
+			{ start: 43, end: 109 }
 		],
 		visible: false
 	},
 	{
-		name: "Keller",
-		vorname: "Wilhelm",
-		kurzbeschreibung: "...",
-		bild: "keller.jpg",
+		name: "",
+		vorname: "Birne",
+		kurzbeschreibung: "Die Birne ist sehr groß. Außerdem ist sie total süß.",
+		bild: "birne.jpg",
+		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vSnnfFbv8ZVqMWuSa74MFl1qNnAND0syu5I4U_B2yweuUTbV2ZnWYpPZ5YwQc52JOm75H66iRyPzO7t/pub", height: 600}],
 		timing: [
-			{ start: 0, end: 0 }
-		],
-		visible: false
-	},
-	{
-		name: "Sachenbacher",
-		vorname: "Anton",
-		kurzbeschreibung: "...",
-		bild: "sachenbacher.jpg",
-		timing: [
-			{ start: 0, end: 0 }
-		],
-		visible: false
-	},
-	{
-		name: "Barnerssoi",
-		vorname: "Georg",
-		kurzbeschreibung: "...",
-		bild: "barnerssoi.jpg",
-		timing: [
-			{ start: 0, end: 0 }
-		],
-		visible: false
-	},
-	{
-		name: "Lederer",
-		vorname: "Carl",
-		kurzbeschreibung: "Ein Böser.",
-		bild: "lederer.jpg",
-		timing: [
-			{ start: 11, end: 22.5 }
-		],
-		visible: false
-	},
-	{
-		name: "Huber-Roethe",
-		vorname: "Ernst",
-		kurzbeschreibung: "Ein Böser.",
-		bild: "huber-roethe.jpg",
-		timing: [
-			{ start: 11, end: 33 }
-		],
-		visible: false
-	},
-	{
-		name: "Ruckdeschel",
-		vorname: "Ludwig",
-		kurzbeschreibung: "Ein Böser.",
-		bild: "ruckdeschel.jpg",
-		timing: [
-			{ start: 11, end: 33 }
+			{ start: 4, end: 109 }
 		],
 		visible: false
 	}
@@ -77,23 +28,59 @@ var vDataCharaktere =
 var vDataOrte =
 [
 	{
-		name: "Die Wiese",
-		adresse: "Schweinbach bei Landshut",
-		kurzbeschreibung: "Irgendeine Wiese in Schweinbach.",
+		name: "Wiese",
+		adresse: "Burg Trausnitz, Landshut",
+		kurzbeschreibung: "Der liebste Aufenhaltsort der beiden.",
 		bild: "wiese.jpg",
-		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vSY_HQF75PgVtHE6Js2oCHUDdC3OO77fytnM-zZWXcmUGCFea_TDKdypcgD-DM3S1rifMgv9rmLs7Tr/pub", height: 3000}],
+		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vTwmf3HLUJ8irSymi_Vf-VZe2_3hvWl90cnuao1XrMnatC-6evhEIwTcep3A1_BzdqDNMIQ1TFZpeCh/pub", height: 600}],
 		timing: [
-			{ start: 3, end: 18 }
+			{ start: 0, end: 9 },
+			{ start: 29, end: 36 },
+			{ start: 43, end: 68 }
 		],
 		visible: false
 	},
 	{
-		name: "Der Wald",
-		adresse: "Rathausprunksaal Landshut",
-		kurzbeschreibung: "Wald im Rathaus.",
-		bild: "wald.jpg",
+		name: "Elternhaus",
+		adresse: "Klinikum rechts der Isar der Technischen Universität München, Ismaninger Straße, München",
+		kurzbeschreibung: "Die beiden sind an benachbarten Ästen aufgewachsen.",
+		bild: "elternhaus.jpg",
+		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vRgj18ElZ42U5nIGILfL7qPJGTCmQUQp2l0DLlQux4gkenljHW5UVyvsZ_y4aoXbanXR2fP9_ANG8EI/pub", height: 600}],
 		timing: [
-			{ start: 18, end: 23 }
+			{ start: 9, end: 19 }
+		],
+		visible: false
+	},
+	{
+		name: "Elternhaus bei Gewitter",
+		kurzbeschreibung: "Auch in schwierigen Situationen, hielten die beiden bereits in früher Kindheit zusammen.",
+		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vT4Y28RsM8NR680QriSuzOsUE0J3tdMmdmP8Qz4M8OZZ1nplxib8DJ-PvKmOz1G-gFQG_aGzcuVbyY7/pub", height: 600}],
+		bild: "elternhaus-gewitter.jpg",
+		timing: [
+			{ start: 19, end: 29 }
+		],
+		visible: false
+	},
+	{
+		name: "Hügel",
+		adresse: "Moniberg, Landshut",
+		kurzbeschreibung: "Diesen Hügel rollten die beiden schon immer gerne hinab.",
+		bild: "huegel.jpg",
+		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vSd7S3Tn-6C-m4vO_o9NQATzIOLc4lj-u44Ffb1JCXUXExlKXPTpFnfNmCZZEYdUpTWPpt5oeYz5UNt/pub", height: 600}],
+		timing: [
+			{ start: 36, end: 43 },
+			{ start: 96, end: 109 }
+		],
+		visible: false
+	},
+	{
+		name: "Nächtliche Natur",
+		adresse: "Carossahöhe, Landshut",
+		kurzbeschreibung: "Hier verbringen die beiden gerne ihre Abende und schauen in die Ferne.",
+		bild: "naechtliche-natur.jpg",
+		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vSeal72aKmdynS3TR7n-hzPw-6DDZzryNsgDotWpxhINW7PmEZceKoV7ropseDTQB4-E7Vtvqc6XwP5/pub", height: 600}],
+		timing: [
+			{ start: 68, end: 96 }
 		],
 		visible: false
 	}
@@ -102,15 +89,26 @@ var vDataOrte =
 var vDataZusatzinfos =
 [
 	{
-		name: "Das ist das übliche Beispielvideo",
-		kurzbeschreibung: "Mittlerweile kommt man gar nicht mehr so leicht an das File ran.",
-		dokument: [{docid: "https://docs.google.com/document/d/e/2PACX-1vSY_HQF75PgVtHE6Js2oCHUDdC3OO77fytnM-zZWXcmUGCFea_TDKdypcgD-DM3S1rifMgv9rmLs7Tr/pub", height: 3000}],
+		name: "Die Aussagen stimmen",
+		kurzbeschreibung: "Birnen sind süßer als Äpfel, dafür haben Äpfel mehr Vitamin C.",
+		dokument: undefined, // Beispiel ohne Dokument
 		bild: "template.png",
 		timing: [
-			{ start: 0, end: 10 }
+			{ start: 73, end: 80 }
+		],
+		visible: false
+	},
+	{
+		name: "Das wars",
+		kurzbeschreibung: "Wie schnell doch die Zeit vergeht.",
+		dokument: undefined, // Beispiel ohne Dokument
+		bild: "template.png",
+		timing: [
+			{ start: 109, end: 150 }
 		],
 		visible: false
 	}
 ];
 
-var vDataProjektbeschreibung = [{docid: "https://docs.google.com/document/d/e/2PACX-1vSY_HQF75PgVtHE6Js2oCHUDdC3OO77fytnM-zZWXcmUGCFea_TDKdypcgD-DM3S1rifMgv9rmLs7Tr/pub", height: 3000}]
+var vDataProjektbeschreibung = 
+[{docid: "https://docs.google.com/document/d/e/2PACX-1vREnt1FQo9Vtu7ZiPH3G05mb4zUSaq9_Bx4oyy-oS0XL_6zNLBoi_xM476i_YWoOS2CVLrGpPz9FFf4/pub", height: 200}]
